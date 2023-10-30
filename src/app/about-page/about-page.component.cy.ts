6e6fceb6561d2dfd62f67611c15e6f637daad97f
Kevin Rodriguez <kevindeywin@gmail.com>
2023-10-29 21:50:14 -0500

    first commit

---
+++ src/app/about-page/about-page.component.cy.ts
@@ -0,0 +1,7 @@
+import { AboutPageComponent } from './about-page.component'
+
+describe('AboutPageComponent', () => {
+  it('should mount', () => {
+    cy.mount(AboutPageComponent)
+  })
+})
---
+++ src/app/about-page/about-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kev-about-page',
+  templateUrl: './about-page.component.html',
+  styleUrls: ['./about-page.component.css']
+})
+export class AboutPageComponent {
+
+}
